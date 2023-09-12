a71dda452d50b61f131335fea48a1e5292dd842b
Omar <ohammad@gwu.edu>
2023-09-12 13:02:24 -0400

    created pieces parent class and piece subclasses

---
+++ Chess/practice-for-ch-ruby-chess-long-practice-main/slideable.rb
@@ -0,0 +1,20 @@
+module Slideable
+    def horizontal_dirs
+    end
+
+    def diagonal_dirs
+    end
+
+    def moves
+    end
+
+    private
+    HORIZONTAL_DIRS = Array.new
+    DIAGONAL_DIRS = Array.new    
+
+    def move_dirs
+    end
+
+    def grow_unblocked_move_in_dir(dx, dy)
+    end
+end
