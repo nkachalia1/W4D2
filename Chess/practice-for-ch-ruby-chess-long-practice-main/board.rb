e8a2edc13ef2d212f5665aac6baf5b51cdd82f2f
Neel <nkachalia1@gmail.com>
2023-09-12 10:38:56 -0400

    Working phase

---
+++ Chess/practice-for-ch-ruby-chess-long-practice-main/board.rb
@@ -0,0 +1,20 @@
+class Board
+    attr_reader :rows
+
+    def initialize
+        @rows = Array.new {Array.new}
+    end
+
+    def [](pos)
+        row, col = pos
+        @rows[row][col]
+    end
+
+    def []=(pos, val)
+        row, col = pos
+        @rows[row][col] = val
+    end
+
+    def move_piece(start_pos, end_pos)
+    end
+end
